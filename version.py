216ea56b4a22395d020752da369e01d0bd3cc3ec
Lukas Sonderegger <sondereggerlukas@gmail.com>
2026-07-27 21:01:59 +0200

    Bump Version v1.0.2

--- version.py
+++ version.py
@@ -1,7 +1,8 @@
-﻿VERSION = "1.0.1"
+﻿VERSION = "1.0.2"
 APP_NAME = "FerdlWorks"
 COMPANY_NAME = "SondereggerSoftware"
 COMPANY_REG_PATH = "SondereggerSoftware"
 GITHUB_REPO = "FerdlWorks"
 GITHUB_OWNER = "soendi"
+
 
